1c070cc6a98a79888299ea2d4f150c0b23c73e3f
issuser <1182639219@qq.com>
2016-04-29 14:06:26 +0800

    Initial Commit

---
+++ voiceTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  voiceTest
+//
+//  Created by issuser on 16/4/29.
+//  Copyright © 2016年 issuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
